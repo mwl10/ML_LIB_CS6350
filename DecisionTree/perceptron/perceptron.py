3a814349992734cb1382f6bcfb52b5e1c8080ae4
mwl10 <mattlowery10@gmail.com>
2023-11-08 17:24:30 -0500

    update perceptron to fix bug in voted

--- DecisionTree/perceptron/perceptron.py
+++ DecisionTree/perceptron/perceptron.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[191]:
+# In[502]:
 
 
 import numpy as np
+np.random.seed(seed=1)
 import pandas as pd
 
 
-# In[192]:
+# ### Process bank-note dataset
+
+# In[503]:
 
 
 train = pd.read_csv('../datasets/bank-note/train.csv', header=None).to_numpy()
 test = pd.read_csv('../datasets/bank-note/test.csv', header=None).to_numpy()
 
 
-# In[193]:
+# In[504]:
 
 
 ### last column is label (-1,1)
 
 x_train = train[:,:-1]
 y_train = train[:,-1]
+y_train = np.where(y_train == 0, -1,1)
+
 x_test = test[:,:-1]
 y_test = test[:,-1]
+y_test= np.where(y_test == 0, -1,1)
 
 # add column of ones to wrap in b
 x_train = np.concatenate((x_train, np.ones((x_train.shape[0],1))), axis=1)
@@ -36,21 +42,22 @@ y_train = y_train[shuffle]
 
 # ### Standard Perceptron
 
-# In[194]:
+# In[505]:
 
 
+### utilities
 calc_avg_error = lambda preds,labels: (preds != labels).mean()
 predict = lambda x,w: np.sign(x @ w)
 
 
-# In[195]:
+# In[506]:
 
 
 # initialize w
-w = np.zeros((x_train.shape[1],))
+w_1 = np.zeros((x_train.shape[1],))
 
 
-# In[196]:
+# In[507]:
 
 
 lr = 0.001
@@ -59,40 +66,48 @@ num_epochs = 10
 for epoch in range(num_epochs):
     for i in range(x_train.shape[0]):
         
-        pred = predict(x_train[i], w)
+        pred = predict(x_train[i], w_1)
         label = y_train[i]
         if pred != label:
             # if we guess negative but answer is positive, increase
             if pred == -1:
-                w += lr * x_train[i]
+                w_1 += lr * x_train[i]
             # if we guess positive but answer is negative, decrease
             else:
-                w -= lr * x_train[i]
+                w_1 -= lr * x_train[i]
         
 
 
-# In[197]:
+# In[508]:
 
 
-train_preds = predict(x_train,w)
-train_avg_error = calc_avg_error(train_preds,y_train)
+train_preds_1 = predict(x_train,w_1)
+train_avg_error_1 = calc_avg_error(train_preds_1,y_train)
 
-test_preds = predict(x_test,w)
-test_avg_error = calc_avg_error(test_preds, y_test)
+test_preds_1 = predict(x_test,w_1)
+test_avg_error_1 = calc_avg_error(test_preds_1, y_test)
 
-print(f'STANDARD PERCEPTRON \n {w=},\n {train_avg_error=},\n {test_avg_error=}')
+print(f'STANDARD PERCEPTRON \n {w_1=},\n {train_avg_error_1=},\n {test_avg_error_1=}')
 
 
 # ### Voted Perceptron
 
-# In[198]:
+# In[509]:
+
+
+### utilities
+calc_avg_error = lambda preds,labels: (preds != labels).mean()
+predict = lambda x,w: np.sign(x @ w)
+
+
+# In[510]:
 
 
 # initialize w
-w = np.zeros((x_train.shape[1],))
+w_2 = np.zeros((x_train.shape[1],))
 
 
-# In[199]:
+# In[511]:
 
 
 lr = 0.001
@@ -105,91 +120,89 @@ c=0
 keeps = []
 for epoch in range(num_epochs):
     for i in range(x_train.shape[0]):
-        
-        pred = predict(x_train[i], w)
+        pred = predict(x_train[i], w_2)
         label = y_train[i]
         if pred != label:
-            keeps.append((w,c))
+            keeps.append((w_2,c))
             c = 0
             
             # if we guess negative but answer is positive, increase
             if pred == -1:
-                w += lr * x_train[i]
+                w_2 = w_2 + (lr * x_train[i])
             # if we guess positive but answer is negative, decrease
             else:
-                w -= lr * x_train[i]
-            
+                w_2 = w_2 - (lr * x_train[i])
         else:
             c += 1
-
 
 def voted_predict(x,keeps):
     sum = np.array([c * predict(x,w) for (w,c) in keeps]).sum(axis=0)
     return np.sign(sum)
 
 
-# In[200]:
+# In[512]:
 
 
-train_preds = voted_predict(x_train,keeps)
-train_avg_error = calc_avg_error(train_preds,y_train)
+train_preds_2 = voted_predict(x_train,keeps)
+train_avg_error_2 = calc_avg_error(train_preds_2,y_train)
 
-test_preds = voted_predict(x_test,keeps)
-test_avg_error = calc_avg_error(test_preds, y_test)
+test_preds_2 = voted_predict(x_test,keeps)
+test_avg_error_2 = calc_avg_error(test_preds_2, y_test)
 
-print(f'VOTED PERCEPTRON \n {train_avg_error=},\n {test_avg_error=} \n KEEPS')
+print(f'VOTED PERCEPTRON \n {train_avg_error_2=},\n {test_avg_error_2=} \n KEEPS')
 [print(f'{w=}, {c=}') for (w,c) in keeps[:10]]
 
 
 # ### Averaged Perceptron
 
-# In[201]:
+# In[517]:
+
+
+### utilities
+calc_avg_error = lambda preds,labels: (preds != labels).mean()
+predict = lambda x,w: np.sign(x @ w)
+
+
+# In[526]:
 
 
 # initialize w
-w = np.zeros((x_train.shape[1],))
+w_3 = np.zeros((x_train.shape[1],))
 
 
-# In[202]:
+# In[527]:
 
 
 lr = 0.001
 num_epochs = 10
-a = np.zeros_like(w)
+a = np.zeros_like(w_3)
 for epoch in range(num_epochs):
     for i in range(x_train.shape[0]):
         
-        pred = predict(x_train[i], w)
+        pred = predict(x_train[i], w_3)
         label = y_train[i]
         if pred != label:
             # if we guess negative but answer is positive, increase
             if pred == -1:
-                w += lr * x_train[i]
+                w_3 += lr * x_train[i]
             # if we guess positive but answer is negative, decrease
             else:
-                w -= lr * x_train[i]
-        a+=w
+                w_3 -= lr * x_train[i]
+        a = a + w_3
         
 a = a / (num_epochs * x_train.shape[0])
 
 
-# In[203]:
+# In[528]:
 
 
-train_preds = predict(x_train,a)
-train_avg_error = calc_avg_error(train_preds,y_train)
+train_preds_3 = predict(x_train,a)
+train_avg_error_3 = calc_avg_error(train_preds_3,y_train)
 
-test_preds = predict(x_test,a)
-test_avg_error = calc_avg_error(test_preds, y_test)
+test_preds_3 = predict(x_test,a)
+test_avg_error_3 = calc_avg_error(test_preds_3, y_test)
 
-print(f'AVERGAGED PERCEPTRON \n {train_avg_error=},\n {test_avg_error=} \n KEEPS')
-
-
-
-# In[ ]:
-
-
-
+print(f'AVERGAGED PERCEPTRON \n {w_3=}\n {a=}\n {train_avg_error_3=},\n {test_avg_error_3=}')
 
 
 # In[ ]:
